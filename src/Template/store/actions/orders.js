7d103614b4f6aae4a560438bbc241fe30b504971
yuslianto <yuslianto56@gmail.com>
2020-01-16 17:46:07 +0700

    http 10 storing orders

--- src/Template/store/actions/orders.js
+++ src/Template/store/actions/orders.js
@@ -1,8 +1,39 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-    return {
-        type: ADD_ORDER,
-        orderData: { items: cartItems, amount: totalAmount }
+    return async dispatch => {
+        const date = new Date();
+        const response = await fetch(
+            `https://theshopapps.firebaseio.com/orders/u1.json`, 
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'aplication/json'
+                },
+                body: JSON.stringify({
+                    cartItems,
+                    totalAmount,
+                    date: date.toISOString()
+                })
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('Something wrent wrong!!');
+        }
+
+        const resData = await response.json();
+
+        dispatch({
+            type: ADD_ORDER,
+            orderData: {
+                id: resData.name, 
+                items: cartItems, 
+                amount: totalAmount, 
+                date: date 
+            
+            }
+        });
+
     };
 };
